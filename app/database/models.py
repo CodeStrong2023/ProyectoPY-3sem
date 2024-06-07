860785b18902964f8a46bc9a20e8b45af9547831
David Abadie <abadiedaviiid@gmail.com>
2024-06-06 21:09:00 -0300

    modifcación bd

--- app/database/models.py
+++ app/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,6 +9,8 @@ class Empleado(Base):
 
     id = Column(Integer, primary_key=True)
     nombre = Column(String(50))
+    apellido = Column(String(50))
+    correo = Column(String(100))
     contrasena = Column(String(50))
     estacionamientos = relationship("Estacionamiento", back_populates="empleado")
 
@@ -29,18 +31,8 @@ class Vehiculo(Base):
     patente = Column(String(10), primary_key=True)
     marca_modelo = Column(String(100))
     telefono_cliente = Column(String(20))
-    tiempo = Column(Integer)
+    hora_entrada = Column(DateTime)
+    hora_salida = Column(DateTime)
     espacio_id = Column(Integer, ForeignKey('estacionamientos.espacio'))
-    usuario_id = Column(Integer, ForeignKey('usuarios.id'))
     estacionamiento = relationship("Estacionamiento", back_populates="vehiculos")
-    usuario = relationship("Usuario", back_populates="vehiculos")
-
-
-class Usuario(Base):
-    __tablename__ = 'usuarios'
-
-    id = Column(Integer, primary_key=True)
-    nombre = Column(String(50))
-    correo = Column(String(100))
-    vehiculos = relationship("Vehiculo", back_populates="usuario")
 
